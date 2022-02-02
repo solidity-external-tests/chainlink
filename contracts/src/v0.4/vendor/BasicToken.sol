111b1540505b0f0e39f42d39d46064cdb7c45b67
Kamil Śliwak <kamil.sliwak@codepoets.it>
2022-02-02 17:55:53 +0100

    Replace `constant` function modifier with `view`

--- contracts/src/v0.4/vendor/BasicToken.sol
+++ contracts/src/v0.4/vendor/BasicToken.sol
@@ -31,7 +31,7 @@ contract BasicToken is linkERC20Basic {
   * @param _owner The address to query the the balance of. 
   * @return balance An uint256 representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) public constant returns (uint256 balance) {
+  function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
   }
 
